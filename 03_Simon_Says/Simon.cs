97c022a66bc276897c9589195c859f63f1c0d686
Acer pici <Acer pici@ACER>
2016-09-12 12:36:29 +0200

    Add project files.

---
+++ 03_Simon_Says/Simon.cs
@@ -0,0 +1,33 @@
+﻿namespace _03_Simon_Says
+{
+    public class Simon
+    {
+
+        public string Echo(string hello)
+        {
+            return hello.ToLower();
+        }
+
+        public string Shout(string ciao)
+        {
+            return ciao.ToUpper();
+        }
+        public string Repeat(string hello)
+        {
+            return hello + " " + hello;
+        }
+        public string Repeat(string hello, int numberoftimes)
+        {
+            string result = hello;
+            for (int i = 1; i < numberoftimes; i++)
+            {  result += " ";
+            result += hello;
+        }
+            return result;
+
+        }
+    }
+}
+        
+    
+
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _03_Simon_Says;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Simon simon = new Simon();
+            Console.WriteLine(simon.Echo("Hello the sun shines"));
+            for (int i = 0; i < 10; i = i + 2)
+            {
+                Console.WriteLine("The value is : " + i);
+                Console.WriteLine("Press any key");
+                Console.ReadKey();
+            }
+        }
+    }
+}
